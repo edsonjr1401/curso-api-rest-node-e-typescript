7e926a2e43ac84b933c3aec5446b0601deeb01f8
Edson Jr <edsoon.jr1401@gmail.com>
2025-09-04 16:30:45 -0300

    att

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,14 +1,18 @@
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 const router = Router();
+
 
 router.get('/', (_, res) => {
     return res.send('Olá, DEV!');
 });
 
-router.get('/teste', (_, res) => {
- return res.send('Teste!')
-});
 
+router.post('/teste', (req, res) => {
+ console.log(req);
+return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
+
+});
 
 export { router };
