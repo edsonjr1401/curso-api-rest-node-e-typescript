a4b5ed2ad086966b6471872e03778dab9c0e36aa
Edson Jr <edsoon.jr1401@gmail.com>
2025-09-06 18:44:57 -0300

    att

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -4,11 +4,13 @@ import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
+  estado: string;
 }
 
 // Schema de validação
 const bodyValidation: yup.Schema<ICidade> = yup.object({
   nome: yup.string().required().min(3),
+  estado: yup.string().required().min(2).max(2)
 }).required();
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
@@ -16,15 +18,24 @@ export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
 
   try {
     // valida req.body
-    validatedData = await bodyValidation.validate(req.body);
-
+    validatedData = await bodyValidation.validate(req.body, { abortEarly:false});
   } catch (error) {
     const yupError = error as yup.ValidationError; 
+    const validationErrors: Record<string, string> = {};
+
+
+    yupError.inner.forEach(error => { 
+      if (error.path === undefined) return;
+      validationErrors[error.path] = error.message
+      });
+    
 
     return res.status(StatusCodes.BAD_REQUEST).json({
-      errors: {
-        default: yupError.message,
-      }
+      errors: validationErrors,
+      
+
+        
+      
     });
   }
 
