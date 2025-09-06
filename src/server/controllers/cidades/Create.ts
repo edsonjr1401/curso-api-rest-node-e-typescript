41592c23634c65fe528d7b91dc351c65330bd503
Edson Jr <edsoon.jr1401@gmail.com>
2025-09-06 17:50:31 -0300

    att

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,20 +1,38 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';  
+import * as yup from 'yup';
 
 interface ICidade {
   nome: string;
 }
 
+// Schema de validação
+const bodyValidation: yup.Schema<ICidade> = yup.object({
+  nome: yup.string().required().min(3),
+}).required();
 
-export const create = (req:Request<[], {}, ICidade>, res:Response) => {
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validatedData: ICidade | undefined = undefined;
 
+  try {
+    // valida req.body
+    validatedData = await bodyValidation.validate(req.body);
 
-console.log(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError; 
 
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: yupError.message,
+      }
+    });
+  }
 
+  // Se passou na validação, já temos os dados corretos
+  console.log(validatedData);
 
+  // Exemplo: acessando a propriedade nome
+  console.log(req.body.nome);
 
-
-
-return res.send('Create!');
-
+  return res.status(StatusCodes.CREATED).send('Create!');
 };
