ac76bd53ca213df0eca91dff17b665d33d14266c
Edson Jr <edsoon.jr1401@gmail.com>
2025-09-07 10:34:14 -0300

    att

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { RequestHandler } from 'express';
 import { StatusCodes } from 'http-status-codes';  
 import * as yup from 'yup';
 
@@ -8,42 +8,27 @@ interface ICidade {
 }
 
 // Schema de validação
-const bodyValidation: yup.Schema<ICidade> = yup.object({
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
   nome: yup.string().required().min(3),
-  estado: yup.string().required().min(2).max(2)
-}).required();
+  estado: yup.string().required().min(2),
+});
 
-export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
-  let validatedData: ICidade | undefined = undefined;
-
+export const createBodyValidator: RequestHandler = (req, res, next) => {
   try {
-    // valida req.body
-    validatedData = await bodyValidation.validate(req.body, { abortEarly:false});
-  } catch (error) {
-    const yupError = error as yup.ValidationError; 
+    // valida de forma síncrona
+    bodyValidation.validateSync(req.body, { abortEarly: false });
+    return next(); // se passou, chama o controller
+  } catch (err) {
+    const yupError = err as yup.ValidationError; 
     const validationErrors: Record<string, string> = {};
-
 
     yupError.inner.forEach(error => { 
       if (error.path === undefined) return;
-      validationErrors[error.path] = error.message
-      });
-    
+      validationErrors[error.path] = error.message;
+    });
 
     return res.status(StatusCodes.BAD_REQUEST).json({
       errors: validationErrors,
-      
-
-        
-      
     });
   }
-
-  // Se passou na validação, já temos os dados corretos
-  console.log(validatedData);
-
-  // Exemplo: acessando a propriedade nome
-  console.log(req.body.nome);
-
-  return res.status(StatusCodes.CREATED).send('Create!');
 };
