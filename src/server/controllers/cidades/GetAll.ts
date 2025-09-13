202488d56dc6af990e464684813ea6fb0362c3a8
Edson Jr <edsoon.jr1401@gmail.com>
2025-09-13 16:14:08 -0300

    att

--- src/server/controllers/cidades/GetAll.ts
+++ src/server/controllers/cidades/GetAll.ts
@@ -24,8 +24,8 @@ export const getAllValidation = validation((getSchema) => ({
 
 export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
   console.log(req.query);
-  res.set.Header('access-control-exponse-headers','x-total-count');
-  res.set.Header('x-total-count', 1);
+  res.setHeader('access-control-exponse-headers','x-total-count');
+  res.setHeader('x-total-count', 1);
 
 
   return res.status(StatusCodes.OK).json([
