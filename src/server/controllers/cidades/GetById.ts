b693d6c39cd1704b7613bbf6f2356cb3b32467f3
Edson Jr <edsoon.jr1401@gmail.com>
2025-09-14 10:56:07 -0300

    att

--- src/server/controllers/cidades/GetById.ts
+++ src/server/controllers/cidades/GetById.ts
@@ -9,14 +9,22 @@ export interface IParamsProps {
   id?: number;
 }
 
-export const getByIdValidation = validation((getSchema) => ({
-  params: getSchema<IParamsProps>(yup.object().shape({
-      id: yup.number().integer().required().moreThan(0),
+export const getByIdValidation = validation(getSchema =>({
+    params: getSchema<IParamsProps>(yup.object().shape({
+    id: yup.number().integer().required().moreThan(0)
     })),
 }));
 
 export const getById = async (req: Request<IParamsProps>, res: Response) => {
-  console.log(req.params);
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!');
-}
+  if(Number(req.params.id) === 99999) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+    errors: {
+     default: 'Registro não encontrado'
+  }
+});
+
+  return res.status(StatusCodes.OK).json({
+    id: req.params.id,
+    nome:'Caxias do Sul',
+  });
+};
